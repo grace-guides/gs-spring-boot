3314e1d83149e34bf064a389f34caada41aa2381
Michael Yan <rain@rainboyan.com>
2024-02-19 15:58:11 +0800

    Using language dynamic modules

--- src/main/groovy/grace/guides/GraceBootApplication.groovy
+++ src/main/groovy/grace/guides/GraceBootApplication.groovy
@@ -1,13 +1,29 @@
 package grace.guides
 
+import grace.guides.plugins.LanguageManager
+import grace.guides.plugins.LanguageModuleDescriptor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class GraceBootApplication {
+class GraceBootApplication implements CommandLineRunner {
+
+	@Autowired
+	LanguageManager languageManager
 
 	static void main(String[] args) {
 		SpringApplication.run(GraceBootApplication, args)
+	}
+
+	@Override
+	void run(String... args) throws Exception {
+		List<LanguageModuleDescriptor> languageModuleDescriptors = this.languageManager.getLanguages()
+
+		languageModuleDescriptors.each { md ->
+			println "Language: key=$md.key, title=$md.title"
+		}
 	}
 
 }
