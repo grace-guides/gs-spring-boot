7ef47ba6248baaa510b1f4a541d194628e122e76
Michael Yan <rain@rainboyan.com>
2024-08-21 20:46:12 +0800

    Upgrade to Grace 2023.0.0
    * Groovy 4.0.22
    
    * Spring Boot 3.3.2
    
    * Migrate to new Dynamic Plugins API

--- src/main/groovy/grace/guides/plugins/LanguageGrailsPlugin.groovy
+++ src/main/groovy/grace/guides/plugins/LanguageGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import grails.util.Environment
 
 class LanguageGrailsPlugin extends DynamicPlugin {
 
-    def grailsVersion = "2022.1.0 > *"
+    def grailsVersion = "2023.0.0 > *"
     def version = "1.0.0"
     def providedModules = [LanguageModuleDescriptor]
 
@@ -15,11 +15,11 @@ class LanguageGrailsPlugin extends DynamicPlugin {
         }
     }
 
-    void doWithDynamicModules() {
+    Closure doWithDynamicModules() { { ->
         // Supported Languages
         language(key: 'en_US', title: 'English', i18nNameKey: 'languages.en_US')
         language(key: 'zh_CN', title: 'Chinese (Simplified Chinese)', i18nNameKey: 'languages.zh_CN')
         language(key: 'zh_TW', title: 'Chinese (Traditional Chinese)', i18nNameKey: 'languages.zh_TW', enabled: true)
-    }
+    }}
 
 }
